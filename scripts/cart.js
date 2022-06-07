bf5a28ba385532e6e66d1a55560e03a8ebc6db78
Yani <yaniveth13@gmail.com>
2022-06-07 18:11:25 -0300

    Funciopn addItemToCart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,7 +1,8 @@
 const addToShoppingCartButtons = document.querySelectorAll(".addToCart");
-console.log(addToShoppingCartButtons);
+const itemContainer = document.querySelector(".cart");
 
-/* Recorremos cada botón y le aplicamos el evento */
+// Recorremos cada botón y le aplicamos el evento
+
 addToShoppingCartButtons.forEach((addToCartBotton) => {
     addToCartBotton.addEventListener("click", addToCartClicked);
 })
@@ -10,11 +11,40 @@ function addToCartClicked(event){
     const button = event.target;
     console.log(button);
 
-    const item = button.closest(".item"); /* Trae los elementos cercanos a esta clase */
+    const item = button.closest(".item"); // Trae los elementos cercanos a esta clase 
     console.log(item);
 
     const itemTitle = item.querySelector('.items__title').textContent;
     console.log(itemTitle);
-    const itemPrice = item.querySelector('.items__price').textContent;
+    const itemPrice = item.querySelector('.price').textContent;
+    console.log(itemPrice);
     const itemImage = item.querySelector('.img').src;
+    console.log(itemImage);
+
+    addItemToCart(itemTitle, itemPrice, itemImage)
+}
+
+// Función que añade al carrito
+
+function addItemToCart(itemTitle, itemPrice, itemImage){
+    console.log(itemTitle, itemPrice, itemImage);
+    const shoppingCartRow = document.createElement("div");
+    const shoppingCartContent = 
+
+        `<div class="items__three item">
+            <div>
+                <img width="40%" src=${itemImage} alt="${itemTitle}">
+            </div>
+            <h4 class="items__title">${itemTitle}</h4>
+            <div class="items__price">
+                <span><strong class="price">${itemPrice}</strong></span>
+            </div>
+        </div>`;
+
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    itemContainer.appendChild(shoppingCartRow);
+
+    // Ocultando p de section cart
+    const cartEmpty = document.querySelector(".empty")
+    cartEmpty.hidden = true;
 }
